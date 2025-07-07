f2cb6e912a4be29f55b1de32a55a8e5d6a63e0ac
NasirSultan <rainasirsultan123@gmail.com>
2025-07-07 16:14:21 +0500

    d

---
+++ index.js
@@ -0,0 +1,40 @@
+import "dotenv/config"
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai"; 
+import { tool } from "@langchain/core/tools";
+import { z } from "zod";
+const llm= new ChatGoogleGenerativeAI({
+  model: "models/gemini-1.5-flash", // or "gemini-pro"
+  temperature: 0.7,
+});
+const calculatorSchema = z.object({
+  operation: z
+    .enum(["add", "subtract", "multiply", "divide"])
+    .describe("The type of operation to execute."),
+  number1: z.number().describe("The first number to operate on."),
+  number2: z.number().describe("The second number to operate on."),
+});
+const calculatorTool = tool(
+  async ({ operation, number1, number2 }) => {
+    // Functions must return strings
+    if (operation === "add") {
+      return `${number1 + number2}`;
+    } else if (operation === "subtract") {
+      return `${number1 - number2}`;
+    } else if (operation === "multiply") {
+      return `${number1 * number2}`;
+    } else if (operation === "divide") {
+      return `${number1 / number2}`;
+    } else {
+      throw new Error("Invalid operation.");
+    }
+  },
+  {
+    name: "calculator",
+    description: "Can perform mathematical operations.",
+    schema: calculatorSchema,
+  }
+);
+const llmWithTools = llm.bindTools([calculatorTool]);
+const res = await llmWithTools.invoke("hello");
+
+console.log(res);
